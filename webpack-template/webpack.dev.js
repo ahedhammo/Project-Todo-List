e007d2224492dac7ff4cc85eceb58859d1675ba1
ahedhammo <244284916+ahedhammo@users.noreply.github.com>
2026-07-23 20:32:37 +0200

    Initial commit

---
+++ webpack-template/webpack.dev.js
@@ -0,0 +1,15 @@
+ import { merge } from 'webpack-merge';
+ import common from './webpack.common.js';
+
+ export default merge(common, {
+   mode: 'development',
+   devtool: 'inline-source-map',
+   devServer: {
+     static: './dist',
+     watchFiles: ['./src/template.html'],
+     open: true,
+     client: {
+       logging: 'warn',
+     },
+   },
+ });
