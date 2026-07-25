15aa1bdc0690df07d47e8b6a97acd505138ea55a
ahedhammo <244284916+ahedhammo@users.noreply.github.com>
2026-07-25 08:53:41 +0200

    Install date-fns package Add dueDate Para

--- webpack-template/src/index.js
+++ webpack-template/src/index.js
@@ -1,10 +1,11 @@
+import { format, isToday, isPast, formatDistanceToNow } from 'date-fns'
 
 class Task {
-  constructor(name, priority, description, project, completed, id) {
+  constructor(name, priority, description, dueDate, completed, id) {
     this.name = name
     this.priority = priority
     this.description = description
-    this.project = project
+    this.dueDate = dueDate
     this.completed = false
     this.id = crypto.randomUUID()
   }
@@ -77,9 +78,10 @@ class ProjectsManager {
 
 const projectBoard = new ProjectsManager()
 
-const code1 = new Task("code1", 1,)
+const code1 = new Task("code1", 1, 'hi', new Date('2026-08-08'))
 const code2 = new Task("code2", 2,)
 const code3 = new Task("code3", 3,)
+const code4 = new Task("code3", 3,)
 
 const project1 = new Project("Project1")
 const project2 = new Project("Project2")
@@ -90,7 +92,8 @@ projectBoard.createProject(project1)
 projectBoard.createProject(project2)
 
 projectBoard.addTaskToProject(project1, code1)
-projectBoard.addTaskToProject(project2, code2)
+projectBoard.addTaskToProject(project2, code3)
+projectBoard.addTaskToProject(project2, code4)
 
 
 
