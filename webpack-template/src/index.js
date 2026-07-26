cf016d6b137bb0767ee052acd59ee9f6af1809be
ahedhammo <12345678+ahedhammo@users.noreply.github.com>
2026-07-26 06:51:58 +0200

    K

--- webpack-template/src/index.js
+++ webpack-template/src/index.js
@@ -2,7 +2,8 @@ import { format, isToday, isPast, formatDistanceToNow } from 'date-fns'
 import ProjectsManager from './projectsManager.js'
 import Project from './project.js'
 import Task from './task.js'
-
+import './style.css'
+import '@fortawesome/fontawesome-free/css/all.min.css'
 
 const projectBoard = new ProjectsManager()
 
